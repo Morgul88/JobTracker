897d082633b96a60f8442af1c9566e400ff5d246
Henrik <staranderh@gmail.com>
2025-09-18 23:06:08 +0200

    init

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,12 +1,1 @@
-﻿
-INSERT INTO Jobs (Title, Description, Company, CreatedAt, UpdatedAt, Notes, Contact, Status)
-VALUES (
-    'Frontend Developer',
-    'Ansökan för frontend jobb',
-    'Acme AB',
-    GETDATE(),
-    NULL,
-    'Skickade CV via email',
-    'hr@acme.com',
-    'Applied'
-);
+﻿SELECT * FROM Jobs WHERE Company = 'Länsstyrelsen'
